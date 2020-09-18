22f059aadcf70f296669179bece2929fd044f9da
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-18 08:50:20 +0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="container">
-    <router-view/>
+  <div>
+    <the-header />
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <router-view />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import TheHeader from '@/components/TheHeader'
 
+export default {
+  components: {
+    TheHeader,
+  },
+}
+</script>
 <style></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link :to="{ name: 'Home' }" class="navbar-brand">Stock Trader</router-link>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <router-link :to="{ name: 'Portfolio' }" tag="li" active-class="active" class="nav-item">
+          <a class="nav-link">Portfolio</a>
+        </router-link>
+        <router-link :to="{ name: 'Stocks' }" tag="li" active-class="active" class="nav-item">
+          <a class="nav-link">Stocks</a>
+        </router-link>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">End day</a>
+        </li>
+        <li class="nav-item">
+          <a 
+            href="#"
+            class="nav-link dropdown-toggle"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >Save & Load</a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a href="#">Save Data</a>
+              <a href="#">Load Data</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+}
+</script>
+<style></style>
