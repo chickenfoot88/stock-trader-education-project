d1392d957d2fa4e32a12728b99d85e7808403eea
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-18 19:57:19 +0300

    Fix sell stock mutation

--- src/store/modules/portfolio/mutations.js
+++ src/store/modules/portfolio/mutations.js
@@ -10,17 +10,17 @@ export default {
         stockPrice
       })
     }
-
+    
     state.funds -= stockPrice * quantity
   },
   
   'SELL_STOCK'(state, { stockId, quantity, stockPrice }) {
-    const record = state.stock.find(item => item.id === stockId)
+    const record = state.stocks.find(item => item.id === stockId)
     if(record.quantity > quantity) {
       record.quantity -= quantity
     } else {
-      const recordIndex = state.stock.indexOf(record)
-      state.stock.splice(recordIndex, 1)
+      const recordIndex = state.stocks.indexOf(record)
+      state.stocks.splice(recordIndex, 1)
       state.funds += stockPrice * quantity
     }
   }
