5b5e422b03038e85bb08a0a428ddf41600c3d677
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-18 19:56:52 +0300

    Remove logging from getters

--- src/store/modules/portfolio/getters.js
+++ src/store/modules/portfolio/getters.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 export default {
   stockPortfolio(state, getters, rooteState, rootGetters) {
-    console.log('getters', state, rootGetters);
     return state.stocks.map(stock => {
       const record = rootGetters['stocks/stocks'].find(item => item.id === stock.id)
       return {
