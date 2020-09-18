f473f14764886f183b22d38dda5aa72a78b0e4fa
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-18 19:57:54 +0300

    Add portfolio stocks

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="">
-    Portfolio page
+  <div class="container pt-4">
+    <div class="row">
+      <div class="col col-sm6 col-md-4 mb-4" v-for="stock in stockPortfolio" :key="stock.id">
+        <stock :stock="stock" />
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import Stock from '@/components/stocks/Stock'
+import { mapGetters } from 'vuex'
+
 export default {
-  name: '',
+  name: 'PortfoioPage',
+  components: {
+    Stock,
+  },
   data() {
-    return {
-      
-    }
+    return {}
+  },
+  computed: {
+    ...mapGetters('portfolio', {
+      stockPortfolio: 'stockPortfolio'
+    })
   }
 }
 </script>
