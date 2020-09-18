258c4cf2500c4e238d9516a590b57f8a458dd74d
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-18 19:38:20 +0300

    Expand stock card for portofilo page

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="card">
     <div class="card-header" style="background-color: #ddf0d7;">
-      <span class="card-title">
-        {{ stock.name }}
-      </span>
+      <span class="card-title">{{ stock.name }}</span>
       <small>(Price: {{ stock.price / 100 }})</small>
+      <small v-if="stock.quantity"> | Quantity: {{ stock.quantity }})</small>
     </div>
     <div class="card-body">
       <div class="d-flex justify-content-between">
         <div>
-          <input type="text" class="form-control" placeholder="Quantity" v-model="quantity">
+          <input type="text" class="form-control" placeholder="Quantity" v-model="quantity" />
         </div>
-        <button class="btn btn-success ml-5" @click="buyStock" :disabled="isBuyButtonDisabled">Buy</button>
+        <button
+          v-if="stock.quantity"
+          class="btn btn-success ml-5"
+          @click="sellStock"
+          :disabled="isBuyButtonDisabled"
+        >Sell</button>
+        <button
+          v-else
+          class="btn btn-success ml-5"
+          @click="buyStock"
+          :disabled="isBuyButtonDisabled"
+        >Buy</button>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'Stock',
   props: {
@@ -29,29 +41,44 @@ export default {
   },
   data() {
     return {
-      quantity: null
+      quantity: null,
     }
   },
   computed: {
     isBuyButtonDisabled() {
       return this.quantity <= 0 || !Number.isInteger(+this.quantity)
-      // return this.quantity <= 0 || !Number.isInteger(parseInt(this.quantity))
-    }
+    },
   },
   methods: {
+    ...mapActions('portfolio', {
+      buyStockAction: 'BUY_STOCK',
+      sellStockAction: 'SELL_STOCK',
+    }),
+
     buyStock() {
       const order = {
         stockId: this.stock.id,
         stockPrise: this.stock.price,
-        quantity: this.quantity
+        quantity: this.quantity,
       }
-      console.log(order);
+      this.buyStockAction(order)
       this.resetQuantity()
     },
+
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrise: this.stock.price,
+        quantity: this.quantity,
+      }
+      this.sellStockAction(order)
+      this.resetQuantity()
+    },
+
     resetQuantity() {
       this.quantity = null
-    }
-  }
+    },
+  },
 }
 </script>
 <style></style>
