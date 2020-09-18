a14bda941f9a8dcf290e9ecbdeb618228c096a4e
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-18 21:05:27 +0300

    Add data to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="home">Home page</div>
+  <div class="container pt-4">
+    <div>
+      <h1>Trade or View you Portfolio</h1>
+      <h6>You may Save & Load your data</h6>
+      <h6>Click on 'End Day' to begin a New Day!</h6>
+      <hr />
+      <p>Your Funds: {{ funds }}</p>
+    </div>
+  </div>
 </template>
-
 <script>
+import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Home',
+  name: 'HomePage',
+  computed: {
+    ...mapGetters('portfolio', ['funds']),
+  },
 }
 </script>
