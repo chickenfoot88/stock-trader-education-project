cd89bd44be1befbc00ffeba981494d948b4fdb22
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-19 08:48:32 +0300

    Add buy and sell restrictions for stocks

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,17 +3,33 @@
     <div class="card-header" :style="{ backgroundColor: stock.quantity ? '#d7edf6' : '#ddf0d7' }">
       <span class="card-title">{{ stock.name }}</span>
       <small>(Price: {{ stock.price / 100 }})</small>
-      <small v-if="stock.quantity"> | Quantity: {{ stock.quantity }})</small>
+      <small v-if="stock.quantity">| Quantity: {{ stock.quantity }})</small>
     </div>
     <div class="card-body">
-      <div class="d-flex justify-content-between">
+      <div class="d-flex justify-content-between align-items-start">
         <div>
-          <input type="text" class="form-control" placeholder="Quantity" v-model="quantity" />
+          <input
+            type="text"
+            class="form-control"
+            :class="{ 'is-invalid': !stock.quantity && insufficiendFunds || stock.quantity && insufficiendQuantity }"
+            placeholder="Quantity"
+            v-model="quantity"
+          />
+          <div
+            v-show="!stock.quantity && insufficiendFunds"
+            class="invalid-feedback"
+          >Insufficient Funds</div>
+
+          <div
+            v-show="stock.quantity && insufficiendQuantity"
+            class="invalid-feedback"
+          >Insufficient Quantity</div>
         </div>
         <button
           v-if="stock.quantity"
           class="btn btn-primary ml-5"
           @click="sellStock"
+          :disabled="insufficiendQuantity"
         >Sell</button>
         <button
           v-else
@@ -26,7 +42,7 @@
   </div>
 </template>
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'Stock',
@@ -44,8 +60,22 @@ export default {
     }
   },
   computed: {
+    ...mapGetters('portfolio', ['funds']),
+
     isBuyButtonDisabled() {
-      return this.quantity <= 0 || !Number.isInteger(+this.quantity)
+      return (
+        this.quantity <= 0 ||
+        !Number.isInteger(+this.quantity) ||
+        this.insufficiendFunds
+      )
+    },
+
+    insufficiendFunds() {
+      return this.quantity * (this.stock.price / 100) > this.funds
+    },
+
+    insufficiendQuantity() {
+      return this.quantity > this.stock.quantity
     },
   },
   methods: {
@@ -58,7 +88,7 @@ export default {
       const order = {
         stockId: this.stock.id,
         stockPrice: this.stock.price,
-        quantity: this.quantity,
+        quantity: +this.quantity,
       }
       this.buyStockAction(order)
       this.resetQuantity()
@@ -68,7 +98,7 @@ export default {
       const order = {
         stockId: this.stock.id,
         stockPrice: this.stock.price,
-        quantity: this.quantity,
+        quantity: +this.quantity,
       }
       this.sellStockAction(order)
       this.resetQuantity()
