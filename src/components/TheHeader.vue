656eecb5635ea9a808834e384035d91b91a51215
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-19 12:12:23 +0300

    Animate save & load buttons

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,33 +4,38 @@
 
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav">
-        <router-link :to="{ name: 'PortfolioPage' }" tag="li" active-class="active" class="nav-item">
+        <router-link
+          :to="{ name: 'PortfolioPage' }"
+          tag="li"
+          active-class="active"
+          class="nav-item"
+        >
           <a class="nav-link">Portfolio</a>
         </router-link>
         <router-link :to="{ name: 'StocksPage' }" tag="li" active-class="active" class="nav-item">
           <a class="nav-link">Stocks</a>
         </router-link>
       </ul>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" @click="endDay">End day</a>
-        </li>
-        <li class="nav-item">
-          <a 
-            href="#"
-            class="nav-link dropdown-toggle"
-            id="navbarDropdown"
-            role="button"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >Save & Load</a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a href="#">Save Data</a>
-              <a href="#">Load Data</a>
-          </div>
-        </li>
-      </ul>
+      <a class="nav-link ml-auto navbar-text" @click="endDay">End day</a>
+      <div class="dropdown">
+        <a
+          class="nav-link dropdown-toggle navbar-text"
+          id="navbarDropdown"
+          role="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          @click="isDropdownOpen = !isDropdownOpen"
+        >Save & Load</a>
+        <div
+          class="dropdown-menu"
+          :class="{ show: isDropdownOpen }"
+          aria-labelledby="navbarDropdown"
+        >
+          <a class="dropdown-item" href="#">Save Data</a>
+          <a class="dropdown-item" href="#">Load Data</a>
+        </div>
+      </div>
       <strong class="navbar-text ml-5">Funds: {{ funds | dollarSign }}</strong>
     </div>
   </nav>
@@ -41,26 +46,26 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   name: '',
   data() {
-    return {}
+    return {
+      isDropdownOpen: false,
+    }
   },
   computed: {
-    ...mapGetters('portfolio', [
-      'funds'
-    ])
+    ...mapGetters('portfolio', ['funds']),
   },
   methods: {
     ...mapActions('stocks', {
-      randomizeStocks: 'RANDOMIZE_STOCKS'
+      randomizeStocks: 'RANDOMIZE_STOCKS',
     }),
 
     endDay() {
       this.randomizeStocks()
-    }
-  }
+    },
+  },
 }
 </script>
 <style>
-  .navbar {
-    z-index: 1;
-  }
+.navbar {
+  z-index: 1;
+}
 </style>
